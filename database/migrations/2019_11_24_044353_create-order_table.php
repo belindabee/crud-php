871a4650c39ce45f511967b604d71ae855dee22a
belindabee <belindabee42@gmail.com>
2019-11-24 11:57:06 +0700

    make migration for crud

---
+++ database/migrations/2019_11_24_044353_create-order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('order_id');
+            $table->integer('customer_id');
+            $table->integer('product_id');
+            $table->decimal('product_price',10,2);
+            $table->mediumInteger('product_qty');
+            $table->timestamps();
+
+            $table->index('customer_id');
+            $table->index('product_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
